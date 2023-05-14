bc121abc8acabea8f3d319866f64e884d78eb4e2
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-05-14 11:36:44 +0100

    Added vercel adapter

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -3,7 +3,7 @@ import devtools from "solid-devtools/vite";
 import solid from "solid-start/vite";
 // @ts-ignore
 import staticAdapter from "solid-start-static";
-
+import vercel from "solid-start-vercel";
 const packages = await (async () => {
   try {
     return (await import("./src/_generated/packages.json")).default;
@@ -23,7 +23,7 @@ export default defineConfig(() => {
         },
       }),
       solid({
-        adapter: staticAdapter(),
+        adapter: vercel({ edge: true }),
         prerenderRoutes: [
           "/",
           ...packages.flatMap(({ name }) => [`/package/${name}`, `/playground/${name}`]),
