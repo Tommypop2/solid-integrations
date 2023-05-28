5bb4bd750cb864d813f80f39fda43d4957327831
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-05-28 23:42:09 +0100

    Added JSDoc docs to qrcode-generator

--- packages/qrcode-generator/src/index.ts
+++ packages/qrcode-generator/src/index.ts
@@ -1,4 +1,4 @@
-import { Accessor, Setter, createSignal, JSX, createMemo } from "solid-js";
+import { Accessor, createMemo } from "solid-js";
 import { isServer } from "solid-js/web";
 import generator from "qrcode-generator";
 type QRCodeOptions = {
@@ -6,22 +6,27 @@ type QRCodeOptions = {
   errorCorrectionLevel: ErrorCorrectionLevel;
 };
 /**
- * A template example of how to create a new primitive.
- *
- * @param param An example of an introductory parameter
- * @return Returns the same parameter as an accessor
+ * A primitive for making a QR code data URL from a string
+ * @param text The text to be encoded into the QR code
+ * @param options Options to be passed to the generator
+ * @returns A data URL containing the QR code as an image
  */
 export const makeQRCode = (
   text: string,
   options: QRCodeOptions = { typeNumber: 4, errorCorrectionLevel: "L" },
 ) => {
-  if(isServer) return;
+  if (isServer) return "";
   const code = generator(options.typeNumber, options.errorCorrectionLevel);
   code.addData(text);
   code.make();
   return code.createDataURL();
 };
-
+/**
+ * A reactive wrapping of {@link makeQRCode}. It returns a memo which contains the data URL.
+ * @param text The text to be encoded into the QR code
+ * @param options Options to be passed to the generator
+ * @returns A memo containing data URL containing the QR code as an image
+ */
 export const createQRCode = (text: Accessor<string>, options?: QRCodeOptions) => {
   return createMemo(() => makeQRCode(text(), options));
 };
