78bd4640d985a12e66a5f3c9a8193054d3228bf3
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-05-29 16:30:38 +0100

    Removed tests Updated lockfile

--- packages/prettier/test/server.test.ts
+++ packages/prettier/test/server.test.ts
@@ -1,9 +1,9 @@
-import { describe, test, expect } from "vitest";
-import { createPrimitiveTemplate } from "../src";
+// import { describe, test, expect } from "vitest";
+// import { createPrimitiveTemplate } from "../src";
 
-describe("createPrimitiveTemplate", () => {
-  test("doesn't break in SSR", () => {
-    const [value, setValue] = createPrimitiveTemplate(true);
-    expect(value(), "initial value should be true").toBe(true);
-  });
-});
+// describe("createPrimitiveTemplate", () => {
+//   test("doesn't break in SSR", () => {
+//     const [value, setValue] = createPrimitiveTemplate(true);
+//     expect(value(), "initial value should be true").toBe(true);
+//   });
+// });
